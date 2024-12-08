c27fd9252c3dd6cafd18a3190da858d71e6e7042
hens <121116111@qq.com>
2024-12-08 15:46:20 +0800

    chore: log

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -4,37 +4,30 @@ import { join } from 'node:path'
 import { UPLOAD_DIR } from '~/constants'
 
 export default defineEventHandler(async (event) => {
-	try {
-		const formData = await readMultipartFormData(event)
-		if (!formData || formData.length === 0) {
-			throw createError({
-				statusCode: 400,
-				message: 'not found file',
-			})
-		}
-
-		const dirId = randomUUID()
-
-		const uploadDir = join(UPLOAD_DIR, dirId)
-		await mkdir(uploadDir, { recursive: true })
-
-		for (const file of formData) {
-			if (file.filename && file.data) {
-				const filePath = join(uploadDir, file.filename)
-				await writeFile(filePath, file.data)
-			}
-		}
-
-		return {
-			success: true,
-			message: 'file upload success',
-			id: dirId,
-		}
-	} catch (error) {
-		console.error(error)
+	const formData = await readMultipartFormData(event)
+	console.log('🚀 ~ defineEventHandler ~ formData:', formData)
+	if (!formData || formData.length === 0) {
 		throw createError({
-			statusCode: 500,
-			message: 'file upload failed',
+			statusCode: 400,
+			message: 'not found file',
 		})
+	}
+
+	const dirId = randomUUID()
+
+	const uploadDir = join(UPLOAD_DIR, dirId)
+	await mkdir(uploadDir, { recursive: true })
+
+	for (const file of formData) {
+		if (file.filename && file.data) {
+			const filePath = join(uploadDir, file.filename)
+			await writeFile(filePath, file.data)
+		}
+	}
+
+	return {
+		success: true,
+		message: 'file upload success',
+		id: dirId,
 	}
 })
