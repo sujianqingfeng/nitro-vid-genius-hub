8178f11c3167acea3ac2e62fc3faf201502821c4
hens <121116111@qq.com>
2024-12-08 14:41:26 +0800

    chore: add logger plugin

--- server/plugins/logger.ts
+++ server/plugins/logger.ts
@@ -1,9 +1,9 @@
 export default defineNitroPlugin((nitroApp) => {
-	nitroApp.hooks.hook("error", async (error, { event }) => {
-		console.error(`${event?.method} ${event?.path} ${error.name} `)
+	nitroApp.hooks.hook('error', async (error, { event }) => {
+		console.error(`${event?.method} ${event?.path} ${error.name} ${error.message}`)
 	})
 
-	nitroApp.hooks.hook("beforeResponse", (e) => {
+	nitroApp.hooks.hook('beforeResponse', (e) => {
 		const status = e.node.res.statusCode
 		console.log(`${e.method} ${e.path} ${status} `)
 	})
